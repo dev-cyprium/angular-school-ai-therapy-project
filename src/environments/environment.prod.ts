c78980929e877bfc140d62639decb3b4636df1bc
Stefan Kupresak <stefan_vg@hotmail.com>
2024-12-12 06:33:20 +0100

    add fly.io url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://github.com/dev-cyprium/angular-school-ai-therapy-project-backend',
+  apiUrl: 'https://angular-school-ai-therapy-project-backend.fly.dev',
 };
